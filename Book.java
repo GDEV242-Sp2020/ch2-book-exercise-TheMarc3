e5bd92aefc3313214f3787ea2573f2eed65f426c
Marc W <60354821+TheMarc3@users.noreply.github.com>
2020-02-09 17:42:56 -0500

    added pages field, accessor, and getter

--- Book.java
+++ Book.java
@@ -1,3 +1,4 @@
+
 /**
  * A class that maintains information on a book.
  * This might form part of a larger application such
@@ -11,15 +12,17 @@ class Book
      // The fields.
     private String author;
     private String title;
+    private int pages;
 
     /**
      * Set the author and title fields when this object
      * is constructed.
      */
-    public Book(String bookAuthor, String bookTitle)
+    public Book(String bookAuthor, String bookTitle, int bookPages)
     {
         author = bookAuthor;
         title = bookTitle;
+        pages = bookPages;
     }
     
     /**
@@ -39,6 +42,14 @@ class Book
     }
     
     /**
+     * returns the book's page count
+     */
+    public int getPages()
+    {
+        return pages;
+    }
+    
+    /**
      * prints the book's author
      */
     public void printAuthor()
@@ -52,6 +63,14 @@ class Book
     public void printTitle()
     {
         System.out.println(getTitle());
+    }
+    
+    /**
+     * prints the book's page count
+     */
+    public void printPages()
+    {
+        System.out.println(getPages());
     }
     
     // Add the methods here ...
